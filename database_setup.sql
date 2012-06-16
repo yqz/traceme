18591cd51f16661c520310c92d26a8246562fcb2
Yunqiao Zhang <zhangyunqiao@gmail.com>
2012-06-16 16:54:14 -0700

    change column name

--- database_setup.sql
+++ database_setup.sql
@@ -1,7 +1,5 @@
 delimiter $$
 
-use traceme$$
-
 CREATE TABLE `TraceRecord` (
       `id` int(11) NOT NULL AUTO_INCREMENT,
       `cid` varchar(48) COLLATE utf8_bin NOT NULL,
@@ -10,7 +8,7 @@ CREATE TABLE `TraceRecord` (
       `latitude` double NOT NULL,
       `timestamp` datetime NOT NULL,
       `altitude` double DEFAULT NULL,
-      `TraceRecordcol` float DEFAULT NULL,
+      `speed` float DEFAULT NULL,
       PRIMARY KEY (`id`),
       KEY `cid_index` (`cid`) USING BTREE
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin$$
