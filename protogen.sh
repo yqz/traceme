9def3114521a2d73cf213b9fb2f97d6d68385791
Yunqiao Zhang <zhangyunqiao@gmail.com>
2012-06-17 09:05:46 -0700

    change protobuf

--- protogen.sh
+++ protogen.sh
@@ -1,2 +1,2 @@
 protoc -I=traceme/server --python_out=traceme/server traceme/server/trace_record.proto
-protoc -I=traceme/server --java_out=. traceme/server/trace_record.proto 
+protoc -I=traceme/server --java_out=android/traceme/src traceme/server/trace_record.proto 
